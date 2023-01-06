36290e68957a03ff211b5f506f64ca7680d7eb9f
tfifthe frank <tfifthefrank@192.168.0.105>
2023-01-06 23:43:10 +0800

    完成bowl的部分代码

--- CS_learning/optimize_algorithms/optimize_algorithm.py
+++ CS_learning/optimize_algorithms/optimize_algorithm.py
@@ -17,46 +17,6 @@ sys.path.append('..')
 from CS_learning.common_func import CommonFunc
 
 
-class logo(Scene):
-    def construct(self):
-        phanton = CommonFunc.add_function(lambda x: 0.3 * np.sin(5 * x), x_range=(-3, 3))
-        start, end = phanton.get_start(), phanton.get_end()
-
-        e_minus = CommonFunc.add_arrow(np.array([-5, 2, 0]), start,
-                                       color=RED, max_tip_length_to_length_ratio=0.05)
-        e_plus = CommonFunc.add_arrow(start, np.array([-5, -2, 0]),
-                                      color=RED, max_tip_length_to_length_ratio=0.05)
-
-        q_average = CommonFunc.add_arrow(np.array([5, 2, 0]), end,
-                                         color=GREEN, max_tip_length_to_length_ratio=0.05)
-        q = CommonFunc.add_arrow(end, np.array([5, -2, 0]),
-                                 color=GREEN, max_tip_length_to_length_ratio=0.05)
-
-        self.play(GrowArrow(e_minus))
-        self.play(GrowArrow(e_plus))
-        self.wait()
-        self.play(Create(phanton))
-        self.wait()
-        self.play(GrowFromPoint(q_average, end))
-        self.play(GrowFromPoint(q, end))
-
-        group = VGroup(phanton, e_minus, e_plus, q_average, q)
-
-        self.play(group.animate.scale(0.6))
-
-        phanton_group = VGroup(*[CommonFunc.add_function(lambda x: 0.3 * np.sin(5 * x - np.pi * 0.5 * dx),
-                                                         x_range=(-3, 3)).scale(0.6) for dx in range(41)])
-
-        text_cn = Text('迷路的小画家', font='HiraginoSansGB-W3').scale(0.7).next_to(group, DOWN)
-
-        self.play(FadeIn(text_cn), run_time=3)
-
-        svg_image = SVGMobject('svg_icon/bird.svg', fill_color=MAROON).scale(1).next_to(phanton, 2 * UP)
-        self.play(SpinInFromNothing(svg_image))
-
-        self.play(Succession(*[Transform(phanton, phanton_group[i]) for i in range(len(phanton_group))], run_time=5))
-
-
 class ThreeDSurfacePlot(ThreeDScene):
     def construct(self):
         resolution_fa = 40
@@ -88,26 +48,29 @@ class ThreeDSurfacePlot(ThreeDScene):
         self.play(Create(gauss_plane))
 
 
-class contour_bowl(ThreeDSurfacePlot):
+class contour_bowl(ThreeDScene):
     # CONFIG = {'inital_start': np.array([-2, 2]),
     #           'axes_x_range': [-4, 4],
     #           'axes_y_range': [-4, 4],
     #           'axes_z_range': [0, 10]}
 
     def construct(self):
-        title = Title('$f(x,y) = x^2 +y^2$')
-        self.play(Write(title))
-        self.wait(3)
-        self.play(Unwrite(title))
         axes, surface = self.get_surfaceplot()
-        l_x, l_y, l_z = self.GD(pow(2, -6)*10, start=[-2, 2])
-        arrows = self.get_dot(axes, l_x, l_y, l_z)
-        self.analysis()
+        self.move_camera(phi=45 * DEGREES, theta=0)
+        l_x, l_y, l_z = self.GD(pow(2, -8) * 20, start=[-2, 2])
+        arrow_vg = self.get_dot(axes, l_x, l_y, l_z)
+        self.analysis(arrow_vg,surface)
+
+    def write_text_before_animate(self, surface):
+        text = MathTex('f(x,y) = x^2 +y^2').next_to(surface, UP+LEFT)
+        self.play(Write(text))
+        self.wait(3)
+        self.play(Unwrite(text))
 
     def get_surfaceplot(self):
 
         def bowl(u, v):
-            z = 1/2 * (np.power(u, 2) + np.power(v, 2))
+            z = 1 / 2 * (np.power(u, 2) + np.power(v, 2))
             return z
 
         axes = ThreeDAxes(x_range=(-4, 4), y_range=(-4, 4), z_range=(0, 8)).shift(np.array([0, 0, -2]))
@@ -122,15 +85,17 @@ class contour_bowl(ThreeDSurfacePlot):
 
         surface_plane.set_fill_by_value(axes=axes, colors=[(RED, 0.0), (YELLOW, 0.2), (BLUE, 4)], axis=2)
 
-        self.set_camera_orientation(phi=75 * DEGREES, theta=0)
-
         self.add(axes)
         self.play(Create(surface_plane))
 
-        for i in range(0, 360, 30):
-            self.move_camera(theta=i * DEGREES)
+        self.write_text_before_animate(surface_plane)
 
-        self.move_camera(phi=45 * DEGREES, theta=0)
+        self.move_camera(phi=75 * DEGREES)
+
+        #self.set_camera_orientation(phi=75 * DEGREES, theta=0)
+
+        for i in range(0, 120, 30):
+            self.move_camera(theta=i * DEGREES)
 
         return axes, surface_plane
 
@@ -140,19 +105,19 @@ class contour_bowl(ThreeDSurfacePlot):
         return np.array([x, y, z])
 
     def write_function(self, surface):
-        tex = MathTex('f(x,y) = x^2+y^2').scale(0.6).next_to(surface, UP+LEFT)
+        tex = MathTex('f(x,y) = x^2+y^2').scale(0.6).next_to(surface, UP + LEFT)
         self.play(Create(tex))
 
     def GD(self, lr, start):
         def partial_x(x):
-            return 1/2 * x
+            return x
 
         def partial_y(y):
-            return 1/2 * y
+            return y
 
         x, y = start[0], start[1]
         GD_x, GD_y, GD_z = [], [], []
-        for it in range(10):
+        for it in range(50):
             GD_x.append(x)
             GD_y.append(y)
             GD_z.append(self.f(x, y)[-1])
@@ -167,16 +132,21 @@ class contour_bowl(ThreeDSurfacePlot):
         self.play(Create(point))
         arrow_vg = VGroup()
         for i in range(1, len(l_x)):
-            arrow = Arrow(start=axes.c2p(l_x[i-1], l_y[i-1], l_z[i-1]),
-                          end=axes.c2p(l_x[i], l_y[i], l_z[i]), color=GREEN, stroke_width=5)
+            arrow = Arrow(start=axes.c2p(l_x[i - 1], l_y[i - 1], l_z[i - 1]),
+                          end=axes.c2p(l_x[i], l_y[i], l_z[i]), color=BLACK, stroke_width=5)
             self.add(arrow)
             self.play(point.animate.move_to(axes.c2p(l_x[i], l_y[i], l_z[i])))
 
             arrow_vg.add(arrow)
         return arrow_vg
 
-    def analysis(self):
-        self.move_camera(phi=0, theta=0)
+    def analysis(self, arrow_vg, surface):
+        #self.move_camera(phi=0, theta=0)
+        self.set_camera_orientation(phi=0, theta=0)
+        s = len(arrow_vg)
+        var = CommonFunc.variable_tracker(label=Text('迭代次数'), start=0, color=RED).scale(0.6).next_to(surface, DOWN+RIGHT)
+        self.add(var)
+        self.play(var.tracker.animate.set_value(s))
 
 
 class saddle_point(ThreeDScene):
@@ -185,7 +155,7 @@ class saddle_point(ThreeDScene):
 
     def get_surfaceplot(self):
         def bowl(u, v):
-            z = np.sin(2*u)*np.cos(2*v)+3
+            z = np.sin(2 * u) * np.cos(2 * v) + 3
             return z
 
         axes = ThreeDAxes(x_range=(-4, 4), y_range=(-4, 4), z_range=(0, 8)).shift(np.array([0, 0, -2]))
@@ -211,4 +181,3 @@ class saddle_point(ThreeDScene):
         self.move_camera(phi=45 * DEGREES, theta=0)
 
         return axes, surface_plane
-
