772bce76a9b7f4716cff4c6504bbfc07a3408b07
tfifthe frank <tfifthefrank@tfifthedeMacBook-Pro.local>
2023-02-22 16:55:31 +0800

    开始极大似然估计

--- CS_learning/maximum_likelihood/maximum_likelihood.py
+++ CS_learning/maximum_likelihood/maximum_likelihood.py
@@ -24,10 +24,12 @@ class NormalDistribution(Scene):
 
         self.write_gaussian()
         self.update_var()
-        #self.render_diagram()
+        # self.render_diagram()
 
     def write_gaussian(self):
-        math_formula = MathTex('f(x)=\\frac{1}{\sigma \sqrt{2 \pi}} e^{-\\frac{1}{2}\left(\\frac{x-\mu}{\sigma}\\right)^2}').to_edge(UP+LEFT)
+        math_formula = MathTex(
+            'f(x)=\\frac{1}{\sigma \sqrt{2 \pi}} e^{-\\frac{1}{2}\left(\\frac{x-\mu}{\sigma}\\right)^2}').to_edge(
+            UP + LEFT)
         self.play(Write(math_formula))
 
     def update_var(self):
@@ -41,17 +43,40 @@ class NormalDistribution(Scene):
 
         self.play(Create(mu), Create(sigma))
 
-        for i, j in [[0, 3],[0, 4]]:
+        for i, j in [[0, 3], [0, 4]]:
             self.mu = i
             self.sigma = j
             self.play(mu.tracker.animate.set_value(self.mu))
             self.play(sigma.tracker.animate.set_value(self.sigma))
-            graph_other = ax.plot(lambda x: self.normal_dis(x, mu=self.mu, sigma=self.sigma), x_range=[-6, 6], use_smoothing=True)
+            graph_other = ax.plot(lambda x: self.normal_dis(x, mu=self.mu, sigma=self.sigma), x_range=[-6, 6],
+                                  use_smoothing=True)
             self.play(Transform(graph, graph_other))
 
     def normal_dis(self, x, sigma, mu):
         coef = 1 / (sigma * np.sqrt(2 * np.pi))
         expon = -1 / 2 * ((x - mu) / sigma) ** 2
-        return coef*np.power(np.e, expon)
+        return coef * np.power(np.e, expon)
+
+
+class Regression(Scene):
+    def construct(self):
+        self.plot_scatter()
+
+    def plot_scatter(self):
+        ax = CommonFunc.add_axes(x_range=[-8, 8], y_range=[-8, 8], x_length=8, y_length=6,
+                                 axis_config={"include_tip": False, "include_numbers": False})
+        self.play(Create(ax))
+
+        x = np.linspace(-7.5, 7.5, 200)
+        gaussian_noise = np.random.normal(size=(200,))
+        y = np.array(x)
+        y_noise = y+gaussian_noise
+        coords = list(zip(x, y_noise))
+
+        #coords = np.random.uniform(-8, 8, size=(10, 2))
+
+        dots = VGroup(*[Dot(ax.c2p(coord[0], coord[1]), radius=0.5*DEFAULT_DOT_RADIUS, color=BLUE) for coord in coords])
+        self.play(FadeIn(dots))
+
 
 
